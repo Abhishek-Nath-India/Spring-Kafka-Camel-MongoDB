7b5c1fd7962c1aba688eff331706558d75f470c1
Abhishek Nath <58810013+Abhishek-Nath-India@users.noreply.github.com>
2021-05-12 20:00:18 +0530

    Initial Commit

---
+++ src/main/java/in/abhisheknath/SpringKafkaCamelMongoDB/SpringKafkaCamelMongoDbApplication.java
@@ -0,0 +1,13 @@
+package in.abhisheknath.SpringKafkaCamelMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaCamelMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaCamelMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/abhisheknath/SpringKafkaCamelMongoDB/SpringKafkaCamelMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package in.abhisheknath.SpringKafkaCamelMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaCamelMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
